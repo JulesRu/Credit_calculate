c31c22b9c37fdcd134441761e2876296f78fbade
JuliaR <donidonic@gmail.com>
2022-12-06 19:03:49 +0300

    add comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
+        // комментарий по округлению:
+        // округлила значения с помощью найденного в сети способа Math.round, корректно ли так делать?
+
         System.out.println();
         System.out.println("Кредит 1000000 на 1 год со ставкой 9.99%, ежемесячный платеж:");
         double a = service.calculate(12);
